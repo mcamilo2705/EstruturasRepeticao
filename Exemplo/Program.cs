431d9df9bbae0f8aeea0b348a45b13f57fb19aee
fic <fic@NOTE10-S28>
2025-03-17 21:55:37 -0300

    Exercicios de repeticao: while, do while e for

---
+++ Exemplo/Program.cs
@@ -0,0 +1,51 @@
+﻿//Estruturas de repeticao
+
+//imprimir todos os numeros de 1 a 5 
+
+//int numero = 1;
+
+////repete uma acao, ENQUANTO algo for verdadeiro
+
+//while (numero <= 5)
+//{
+//    Console.WriteLine($"Numero: {numero}");
+//    //operacao de incremento
+//    //numero = numero + 1;
+//    numero++;
+//}
+
+//Enquanto
+
+//Console.Write("Informe a idade: ");
+//int idade = int.Parse(Console.ReadLine());
+
+//while (idade != 22)
+//{
+//    Console.Write("Informe a idade: ");
+//    idade = int.Parse(Console.ReadLine());
+//}
+
+//Faca - Enquanto (DO/WHILE)
+
+
+//int idade;
+//do
+//{
+//    Console.Write("Informe a idade: ");
+//    idade = int.Parse(Console.ReadLine());
+//}
+//while (idade != 22);
+
+//Para - FOR
+//Quero imprimir os numeros do 1 ao 5
+
+//1- cria uma variavel chamada "i"(indice)
+//2- faz a comparacao de i com 5
+//3- incremento
+//for (int i = 0; i < 6; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+//### **1. Contagem de 1 a 20**
---
+++ Exercicios/Program.cs
@@ -0,0 +1,61 @@
+﻿
+//Crie um programa que exiba os números de 1 a 20 na tela.
+
+using System.Net.Mail;
+
+for (int i = 0; i <= 20; i++)
+{
+    Console.WriteLine(i);
+}
+
+//Peça ao usuário para inserir a senha "1234". Continue pedindo até que ele acerte.
+
+string senha;
+
+do
+{
+    Console.Write("Informe a senha: ");
+    senha = Console.ReadLine();
+}
+while (senha != "1234");
+
+Console.WriteLine("Senha correta !");
+
+//Peça ao usuário um número e exiba a tabuada dele de 1 a 10.
+
+int numero = 0;
+
+Console.Write("Digite o numero: ");
+numero = int.Parse(Console.ReadLine());
+Console.WriteLine($"#### TABUADA DO {numero} ####");
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine($"[{numero}] X [{i}] = {numero*i} ");
+}
+
+//Peça um número ao usuário e faça uma contagem regressiva até 0.
+
+int n1;
+Console.Write("Digite o numero: ");
+n1 = int.Parse(Console.ReadLine());
+
+for (int i = n1; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
+
+//Solicite notas ao usuário (entre 0 e 10) até que ele insira um número inválido. Depois, exiba a média das notas.
+
+int nota = 0;
+int cont = 0;
+Console.Write("Digite a nota: ");
+nota = int.Parse(Console.ReadLine());
+
+while (nota < 11)
+{
+    Console.Write("Digite a nota: ");
+    nota = int.Parse(Console.ReadLine());
+    nota += nota;
+    cont++;
+}
+Console.WriteLine($"A media de {cont} notas e {nota}");
